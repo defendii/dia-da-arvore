9817f9812458840e6572b15d778b754d5e07e49f
Ana Julia <130059236+defendii@users.noreply.github.com>
2023-09-21 09:12:04 -0300

    conseguindo
    conseguindo

---
+++ aulinha/ex03.js
@@ -0,0 +1,8 @@
+let salario = 1000
+console.log("Salário original: " + salario)
+
+salario = salario * 1.15
+console.log("Salário com aumento: " + salario)
+
+salario = salario - (salario * 0.08)
+console.log("Salário com desconto: " + salario)
